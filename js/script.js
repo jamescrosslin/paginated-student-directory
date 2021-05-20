51bf57bb84cf83e8db94b959fb73b5e6e56294b7
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-20 12:51:54 -0700

    refactored pagination

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const linkList = document.querySelector(".link-list");
-const studentList = document.querySelector(".student-list");
-const header = document.querySelector("header");
+const linkList = document.querySelector('.link-list');
+const studentList = document.querySelector('.student-list');
+const header = document.querySelector('header');
 
 /**
  * @function addSearch
@@ -13,7 +13,7 @@ function addSearch() {
     <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
   </label>
   `;
-  header.insertAdjacentHTML("beforeend", searchBar);
+  header.insertAdjacentHTML('beforeend', searchBar);
 }
 
 /**
@@ -24,14 +24,14 @@ function addSearch() {
  *    and posts them to the DOM
  */
 function showPage(list, page) {
-  const startIndex = page * 9 - 9;
   const endIndex = page * 9;
-  const ul = document.querySelector(".student-list");
-  ul.innerHTML = "";
+  const startIndex = endIndex - 9;
+  const ul = document.querySelector('.student-list');
+  ul.innerHTML = '';
   for (let i = 0; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
       ul.insertAdjacentHTML(
-        "beforeend",
+        'beforeend',
         `<li class="student-item cf">
         <div class="student-details">
           <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
@@ -41,7 +41,7 @@ function showPage(list, page) {
         <div class="joined-details">
           <span class="date">Joined ${list[i].registered.date}</span>
         </div>
-      </li>`
+      </li>`,
       );
     }
   }
@@ -53,7 +53,7 @@ function showPage(list, page) {
  * @description creates the correct number of page buttons
  */
 function addPagination(list) {
-  linkList.innerHTML = "";
+  linkList.innerHTML = '';
 
   let i = 1;
   do {
@@ -62,10 +62,10 @@ function addPagination(list) {
         <button type="button">${i}</button>
       </li>
     `;
-    linkList.insertAdjacentHTML("beforeend", button);
+    linkList.insertAdjacentHTML('beforeend', button);
     i++;
   } while (i <= Math.ceil(list.length / 9));
-  linkList.firstElementChild.firstElementChild.className = "active";
+  linkList.firstElementChild.firstElementChild.className = 'active';
 }
 
 /**
@@ -74,12 +74,9 @@ function addPagination(list) {
  * @description clears list elements from page and displays lack of matches
  */
 function addNoResults() {
-  studentList.innerHTML = "";
-  linkList.innerHTML = "";
-  studentList.insertAdjacentHTML(
-    "beforeend",
-    `<h3 class="no-results">No results found</h3>`
-  );
+  studentList.innerHTML = '';
+  linkList.innerHTML = '';
+  studentList.insertAdjacentHTML('beforeend', `<h3 class="no-results">No results found</h3>`);
   return false;
 }
 
@@ -89,9 +86,9 @@ function addNoResults() {
  * @description checks search field against names of all students
  */
 function searchPage(list) {
-  const searchText = document.getElementById("search").value.toLowerCase();
+  const searchText = document.getElementById('search').value.toLowerCase();
   const matchList = list.filter((student) => {
-    const name = student.name.first + " " + student.name.last;
+    const name = student.name.first + ' ' + student.name.last;
     return name.toLowerCase().includes(searchText);
   });
   return matchList.length ? matchList : addNoResults();
@@ -101,13 +98,11 @@ function searchPage(list) {
  * @listens linkList
  * @description handles display change on a page button click
  */
-linkList.addEventListener("click", (e) => {
+linkList.addEventListener('click', (e) => {
   const button = e.target;
-  if (button.tagName === "BUTTON") {
-    document
-      .querySelectorAll("li button")
-      .forEach((button) => (button.className = ""));
-    button.className = "active";
+  if (button.tagName === 'BUTTON') {
+    document.querySelectorAll('li button').forEach((button) => (button.className = ''));
+    button.className = 'active';
     const list = searchPage(data);
     if (list) showPage(list, +button.textContent);
   }
@@ -118,8 +113,8 @@ linkList.addEventListener("click", (e) => {
  * @description on a button or search image click, calls a search of the page
  * and display change
  */
-header.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON" || e.target.tagName === "IMG") {
+header.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON' || e.target.tagName === 'IMG') {
     const list = searchPage(data);
     if (list) {
       showPage(list, 1);
@@ -132,7 +127,7 @@ header.addEventListener("click", (e) => {
  * @listens header
  * @description on a keyup, calls a search of the page and display change
  */
-header.addEventListener("keyup", (e) => {
+header.addEventListener('keyup', (e) => {
   const list = searchPage(data);
   if (list) {
     showPage(list, 1);
